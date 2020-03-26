e1555287379d40c136c932d1c351cc9b51aff864
SergStas <sergstas0@gmail.com>
2020-03-26 22:32:25 +0500

    Added bot's class

---
+++ SemesterWork/MainForm/Bot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MainForm
+{
+    class Bot
+    {
+        public int Id { get; }
+        public Point Position { get; private set; }
+        public BotProgram BotProgram { get; private set; }
+
+        public Bot(int id)
+        {
+            Id = id;
+            BotProgram = new BotProgram();
+        }
+
+        public Bot(int id, Point pos) : this(id)
+        {
+            Position = pos;
+        }
+    }
+}
---
+++ SemesterWork/MainForm/BotProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MainForm
+{
+    class BotProgram
+    {
+        public const int Size = 64;
+        public int[] Programs { get; private set; }
+
+        Random random;
+
+        static Dictionary<int, Action<Bot>> commands = new Dictionary<int, Action<Bot>>();
+
+        public BotProgram()
+        {
+            random = new Random();
+            Programs = Programs.Select(x => random.Next(0, Size)).ToArray();
+        }
+
+        public static void RegisterCommand(int number, Action<Bot> action)
+        {
+            commands.Add(number, action);
+        }
+
+    }
+}
